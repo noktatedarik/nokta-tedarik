75254085cb845f95f398c8d932f14b679d865f41
altindagosman600 <altindagosman600@gmail.com>
2026-08-09 11:58:52 +0300

    Yonetici paneli: ciro raporu, grafikler (urun dagilimi, il bazli, son 7 gun) ve sure/rota takibi eklendi

---
+++ fiyatlar.js
@@ -0,0 +1,9 @@
+var FIYATLAR = {
+  guncelleme: '09.08.2026',
+  TAMKOY: {
+    D: 28, E: 28, F: 28, G: 28, H: 28, I: 32, J: 0, K: 28
+  },
+  BAGETTA: {
+    D: 28, E: 28, F: 28, G: 28, H: 28, I: 28, J: 28, K: 28
+  }
+};
