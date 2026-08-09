2d7cb39a1f950eaba96464b93718f69cadbc9e8a
altindagosman600 <altindagosman600@gmail.com>
2026-08-09 12:26:08 +0300

    Cache duzeltmesi: service worker network-first, updateViaCache none - yeni tasarim mobilde her zaman guncel gorunsun

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
+var SURUM = 'v3-premium';
 self.addEventListener('install', function () {
+  caches.keys().then(function (anahtarlar) {
+    return Promise.all(anahtarlar.map(function (k) { return caches.delete(k); }));
+  });
   self.skipWaiting();
 });
 self.addEventListener('activate', function (e) {
   e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  var url = e.request.url;
+  if (url.indexOf('github.io') === -1 && url.indexOf('localhost') === -1 && url.indexOf('file:') !== 0) return;
+  e.respondWith(
+    fetch(e.request).then(function (yanit) {
+      var klon = yanit.clone();
+      caches.open(SURUM).then(function (c) { c.put(e.request, klon); }).catch(function () { });
+      return yanit;
+    }).catch(function () {
+      return caches.match(e.request);
+    })
+  );
 });
 self.addEventListener('notificationclick', function (e) {
   e.notification.close();
